648eacf5db22d0c0cee6534f9f7a75d7c5837318
NRLacerda <nrochalacerda@gmail.com>
2022-07-27 15:03:37 -0300

    a

--- Arraia/Program.cs
+++ Arraia/Program.cs
@@ -4,14 +4,28 @@
     {
         static void Main(string[] args)
         {
+            // Lembre-se, isso aqui é só um arquivo que utilizei pra testar como funcionam, e se sei fazer 
+            // algumas funcionalidades do .NET e C#.
+            // Espero que no futuro eu mude o mundo com programação, e programação tenha mudado minha vida
+            // = Nicolas Rocha Lacerda, 14:25 - 27/07/2022.
             int a;
             int[,] poker = { { 1, 2, 3 }, { 4, 5, 6 } };
 
             System.Console.WriteLine("Essa arraia tem {0} dimensões.", poker.Rank);
-            Console.WriteLine("Write a number");
+            System.Console.WriteLine(poker.ToString()); 
+            Console.WriteLine("Escreva um número");
             a = int.Parse(Console.ReadLine());
-            for (int b = 0; b < a; b++)
-                Console.WriteLine(b);
+            //  Tenho que aprender a utilizar For dentro de While
+            int b = 0;
+            {
+                do
+                {
+
+                    Console.WriteLine(b);
+                    b++;
+
+                } while (b < a);
+            }
         }
     }
 }
