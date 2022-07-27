b2e604b0d17217a431c2693f1c186e8ca9bc8f69
NRLacerda <nrochalacerda@gmail.com>
2022-07-27 11:59:46 -0300

    att

---
+++ Arraia/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Arraia
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int[,] poker = { { 1, 2, 3 }, { 4, 5, 6 } };
+
+            System.Console.WriteLine("Essa arraia tem {0} dimensões.", poker.Rank);
+            Console.WriteLine("Write a number");
+            a = int.Parse(Console.ReadLine());
+            for (int b = 0; b < a; b++)
+                Console.WriteLine(b);
+        }
+    }
+}
